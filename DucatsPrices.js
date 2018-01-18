890fc45f3fea8085c31e5558658d6e8014edc553
cacapis <cacapis@users.noreply.github.com>
2018-01-18 17:29:59 -0300

    Create DucatsPrices.js

---
+++ DucatsPrices.js
@@ -0,0 +1,67 @@
+// Warframes
+var BANSHEEPRIME = {Blueprint:45, Chassis:100, Neuroptics:15, Systems:100};
+var HYDROIDPRIME = {Blueprint:45, Chassis:15, Neuroptics:45, Systems:100};
+var MIRAGEPRIME = {Blueprint:100, Chassis:15, Neuroptics:15, Systems:45};
+var NEKROSPRIME = {Blueprint:100, Chassis:15, Neuroptics:45, Systems:100};
+var OBERONPRIME = {Blueprint:45, Chassis:15, Neuroptics:100, Systems:100};
+var VALKYRPRIME = {Blueprint:15, Chassis:100, Neuroptics:45, Systems:100};
+var VAUBANPRIME = {Blueprint:100, Chassis:100, Neuroptics:100, Systems:100};
+// Warframes - Vaulted
+var ASHPRIME = {Blueprint:45, Chassis:15, Neuroptics:45, Systems:65};
+var EMBERPRIME = {Blueprint:100, Chassis:15, Neuroptics:15, Systems:45};
+var FROSTPRIME = {Blueprint:100, Chassis:15, Neuroptics:15, Systems:45};
+var MAGPRIME = {Blueprint:100, Chassis:45, Neuroptics:15, Systems:15};
+var NOVAPRIME = {Blueprint:45, Chassis:100, Neuroptics:15, Systems:15};
+var NYXPRIME = {Blueprint:15, Chassis:100, Neuroptics:100, Systems:45};
+var RHINOPRIME = {Blueprint:100, Chassis:65, Neuroptics:45, Systems:15};
+var SARYNPRIME = {Blueprint:65, Chassis:100, Neuroptics:45, Systems:15};
+var TRINITYPRIME = {Blueprint:45, Chassis:45, Neuroptics:25, Systems:15};
+var VOLTPRIME = {Blueprint:45, Chassis:45, Neuroptics:45, Systems:45};
+
+// Sentinels
+var HELIOSPRIME = {Blueprint:45, Carapace:15, Cerebrum:100, Systems:45};
+// Sentinels - Vaulted
+var CARRIERPRIME = {Blueprint:15, Carapace:15, Cerebrum:65, Systems:15};
+
+// Weapons
+var AKBOLTOPRIME = {Blueprint:15, Barrel:45, Link:45, Receiver:100};
+var AKBRONCOPRIME = {Blueprint:15, Link:45};
+var AKSTILETTOPRIME = {Blueprint:100, Barrel:45, Link:45, Receiver:45};
+var BALLISTICAPRIME = {Blueprint:100, "Lower Limb":15, Receiver:45, String:45, "Upper Limb":45};
+var BRATONPRIME = {Blueprint:25, Barrel:15, Receiver:45, Stock:15};
+var BRONCOPRIME = {Blueprint:15, Barrel:45, Receiver:15};
+var BURSTONPRIME = {Blueprint:15, Barrel:45, Receiver:15, Stock:15};
+var CERNOSPRIME = {Blueprint:45, Grip:15, "Lower Limb":100, String:45, "Upper Limb":15};
+var EUPHONAPRIME = {Blueprint:15, Barrel:45, Receiver:100};
+var FANGPRIME = {Blueprint:15, Blade:15, Handle:25};
+var FRAGORPRIME = {Blueprint:65, Handle:65, Head:15};
+var GALATINEPRIME = {Blueprint:100, Blade:15, Handle:45};
+var KOGAKEPRIME = {Blueprint:45, Boot:15, Gauntlet:100};
+var LEXPRIME = {Blueprint:25, Barrel:15, Receiver:15};
+var NAMISKYLAPRIME = {Blueprint:15, Blade:100, Handle:45};
+var ORTHOSPRIME = {Blueprint:45, Blade:45, Handle:15};
+var PARISPRIME = {Blueprint:15, Grip:45, "Lower Limb":15, String:15, "Upper Limb":25};
+var SILVAAEGISPRIME = {Blueprint:45, Blade:45, Guard:100, Hilt:15};
+var SYBARISPRIME = {Blueprint:15, Barrel:100, Receiver:45, Stock:15};
+var TIGRISPRIME = {Blueprint:100, Barrel:45, Receiver:45, Stock:15};
+var VENKAPRIME = {Blueprint:45, Blades:15, Gauntlet:100};
+// Weapons - Vaulted
+var AKLEXPRIME = {Blueprint:45, Link:100};
+var ANKYROSPRIME = {Blueprint:15, Blade:65, Gauntlet:15};
+var BOARPRIME = {Blueprint:15, Barrel:45, Receiver:15, Stock:100};
+var BOLTORPRIME = {Blueprint:100, Barrel:15, Receiver:45, Stock:15};
+var DAKRAPRIME = {Blueprint:45, Blade:65, Handle:15};
+var DUALKAMASPRIME = {Blueprint:15, Blade:100, Handle:45};
+var GLAIVEPRIME = {Blueprint:100, Blade:45, Disc:45};
+var HIKOUPRIME = {Blueprint:15, Pouch:15, Stars:15};
+var KAVASAPRIME = {Blueprint:45, Band:45, Buckle:65};
+var LATRONPRIME = {Blueprint:15, Barrel:15, Receiver:15, Stock:15};
+var NIKANAPRIME = {Blueprint:65, Blade:100, Hilt:100};
+var ODONATAPRIME = {Blueprint:45, Harness:15, Systems:15, Wings:45};
+var REAPERPRIME = {Blueprint:15, Blade:45, Handle:15};
+var SCINDOPRIME = {Blueprint:45, Blade:100, Handle:45};
+var SICARUSPRIME = {Blueprint:15, Barrel:15, Receiver:100};
+var SOMAPRIME = {Blueprint:15, Barrel:15, Receiver:45, Stock:100};
+var SPIRAPRIME = {Blueprint:15, Blade:100, Pouch:100};
+var VASTOPRIME = {Blueprint:45, Barrel:15, Receiver:15};
+var VECTISPRIME = {Blueprint:45, Barrel:15, Receiver:100, Stock:65};
